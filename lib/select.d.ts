8f9f465ba5acd770790389f851bfdd229de4e4ae
luigisenzamici <luigisenzamici@outlook.it>
2018-10-18 12:39:18 +0200

    first implementation

---
+++ lib/select.d.ts
@@ -0,0 +1,38 @@
+export declare class Select {
+    private Root;
+    private Name;
+    private Index;
+    private Display;
+    private PreselectIndex?;
+    private Placeholder;
+    private Data?;
+    private Id?;
+    private Class?;
+    private Style?;
+    private node;
+    private updated;
+    private changed;
+    private selected;
+    private sel;
+    updated$: import("rxjs/internal/Observable").Observable<any>;
+    changed$: import("rxjs/internal/Observable").Observable<any>;
+    selected$: import("rxjs/internal/Observable").Observable<any>;
+    selectedIndex: any;
+    selectedDisplay: any;
+    constructor(Root: HTMLElement, Name: string, Index: string, Display: string, PreselectIndex?: string, Placeholder?: string, Data?: Array<any>, Id?: string, Class?: string, Style?: string);
+    private init;
+    private create;
+    private option;
+    private placeholder;
+    private populate;
+    private unselAll;
+    private selItem;
+    isSelected: () => boolean;
+    unselect: () => void;
+    select: (Index: any) => void;
+    setData: (data: any[]) => void;
+    setClass: (Class: string) => void;
+    removeClass: (Class: string) => void;
+    setStyle: (Style: string) => void;
+    removeStyle: () => void;
+}
---
+++ src/select.ts
@@ -0,0 +1,130 @@
+import { htmlParse, empty} from '@tspower/core';
+import { Subject, timer } from 'rxjs';
+
+
+
+export class Select{
+    private node:HTMLSelectElement;
+    private updated = new Subject<any>();
+    private changed = new Subject<any>();
+    private selected = new Subject<any>();
+    private sel:boolean = false;
+
+    public updated$ = this.updated.asObservable();
+    public changed$ = this.changed.asObservable();
+    public selected$ = this.selected.asObservable();
+
+    public selectedIndex:any;
+    public selectedDisplay:any;
+    
+    
+
+    constructor(
+        private Root:HTMLElement,
+        private Name:string,
+        private Index:string,
+        private Display:string,
+        private PreselectIndex?:string,
+        private Placeholder:string = "Select an Item",
+        private Data?:Array<any>,
+        private Id?:string,
+        private Class?:string,
+        private Style?:string
+    ){
+        this.create();
+        this.init();
+    }
+    private init = ()=>{
+
+        this.node.addEventListener("change", (event)=>{
+            this.selectedIndex = this.node.options[this.node.selectedIndex].value;
+            this.selectedDisplay = this.node.options[this.node.selectedIndex].text;
+            let option = this.node.options.item(this.node.selectedIndex);
+            option.setAttribute("selected", '');
+
+            if(this.selectedIndex =="unselect") {
+                this.selected.next(false);
+                this.sel = false;
+            }else{ 
+                this.sel = true;
+                this.selected.next(true);
+            }
+            this.changed.next({index:this.selectedIndex, display:this.selectedDisplay});
+        });
+
+    }
+
+    private create = () =>{
+        let id = (this.Id)?`id="${this.Id}"`:null;
+        let classe = (this.Class)?`class="${this.Class}"`:null;
+        let stile = (this.Style)?`style="${this.Style}"`:null;
+
+        let template = `<select name="${this.Name}" ${(id)?id:""} ${(classe)?classe:""} ${(stile)?stile:""} >
+                        </select>`;
+
+        this.node = htmlParse(template) as HTMLSelectElement;
+
+        if(this.Data){
+            this.populate();
+        }
+        this.Root.appendChild(this.node);
+
+    }
+    private option = (Index:string, Display:string, preset?:boolean):HTMLOptionElement=>{
+        let sel = (preset)?"selected":"";
+        let template = `<option value="${Index}" ${sel} > ${Display} </option>`;
+        return htmlParse(template) as HTMLOptionElement;
+    }
+    private placeholder = ():HTMLOptionElement=> {
+        let pre = (this.PreselectIndex=="unselect" || this.PreselectIndex==null)?"selected":null;
+        return htmlParse(`<option value="unselect" ${(pre!=null)?pre:""}>${this.Placeholder}</option>`) as HTMLOptionElement;
+    }
+
+    private populate = (atStart:boolean = true)=>{
+       empty(this.node);
+       this.node.appendChild(this.placeholder());
+        this.Data.forEach((e, i)=>{
+                let pres_index = (atStart)? (e[this.Index]==this.PreselectIndex)?true:false :null;
+                 this.node.appendChild(this.option(e[this.Index], e[this.Display], pres_index));
+        });
+    }
+
+    private unselAll = ():void=>{
+        let s = this.node.querySelector("option[selected]");
+        s.removeAttribute("selected");
+    }
+    private selItem =(value:string):void=>{
+        this.unselAll();
+        let option = this.node.querySelector(`option[value="${value}"]`);
+        option.setAttribute("selected", '');
+    }
+
+    //shared method
+    isSelected = ():boolean=> this.sel;
+
+    unselect =():void=>{
+        this.selItem("unselect");
+    }
+
+    select = (Index:any):void=>{
+        this.selItem(Index);
+    }
+
+    setData = (data:Array<any>):void=>{
+        this.Data = data;
+        this.populate(false);
+        this.updated.next(true);
+    }
+    setClass = (Class:string)=>{
+        this.node.classList.add(Class);
+    }
+    removeClass = (Class:string)=>{
+        this.node.classList.remove(Class);
+    }
+    setStyle = (Style:string)=>{
+        this.node.setAttribute("style", Style);
+    }
+    removeStyle = ()=>{
+        this.node.removeAttribute("style");
+    }
+}
